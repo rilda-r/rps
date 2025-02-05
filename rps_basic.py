619300a8cf4db3a04637f24d4aa52ec67eea9ad7
RILDA_R <rilda.rmm@gmail.com>
2025-02-05 15:10:24 +0530

    Update rps_basic.py

--- rps_basic.py
+++ rps_basic.py
@@ -1,22 +1,60 @@
 #ROCK PAPER SCISSORS
 
-#random.choice()
-
+import tkinter as tk
 import random
-inputs =['rock','paper','scissors']
-user_choice=input('your choice:').lower()
-if user_choice not in inputs:
-    print('invalid input. Try  again')
-else:
-    comp_choice=random.choice(inputs)
-    print(comp_choice)
-    if comp_choice==user_choice:
-        print('its a draw')
-    elif comp_choice=='rock' and user_choice=='scissors':
-        print('you lose:P')
-    elif comp_choice=='paper' and user_choice=='rock':
-        print('you lose:P')
-    elif comp_choice=='scissors' and user_choice=='paper':
-        print('you lose:P')
+from tkhtmlview import HTMLLabel
+
+def gaming():
+    user = user_choice.get()
+    print('user:',user)
+
+    choice = ['rock','paper','scissors']
+    comp_choice = random.choices(choice)[0]
+    print('comp:',comp_choice)
+    
+    if user.lower() in choice:
+        if comp_choice == user.lower():
+            l=tk.Label(text='draw')
+            print('draw')
+
+        elif (comp_choice == 'rock' and user.lower() == 'paper') or (comp_choice == 'paper' and user.lower() == 'scissors'):
+            l=tk.Label(text='user wins')
+            print('user wins')
+
+        else:
+            l=tk.Label(text='comp wins')
+            print('comp wins')
     else:
-        print('you have won!')
+        l=tk.Label(text='wrong input. Change the input!')
+        print('wrong input!')
+    
+    l.pack()    
+    
+        
+
+window = tk.Tk()
+window.title('rock paper scissors')
+window.geometry('1270x680')
+
+line = HTMLLabel(window, html = "\
+                 <h1 style='color:red;'><b>hi! ITs the rock paper scissors game!</b></h1>\
+                 <img src='https://i.ytimg.com/vi/Z13kiomZvak/maxresdefault.jpg'>\
+                 <h2>Give your input</h2><h6>rock/paper/scissors</h6>\
+                 ")
+
+
+line.pack(fill='both',expand='true')
+
+l1 = tk.Label(text='user-choice: ')
+l1.pack()
+user_choice = tk.StringVar()
+e1 = tk.Entry(textvariable = user_choice)
+e1.pack()
+
+
+
+b1 = tk.Button(text = 'result', command = gaming)
+b2 = tk.Button(text = 'end game', command = exit)
+b1.pack()
+b2.pack()
+window.mainloop()
